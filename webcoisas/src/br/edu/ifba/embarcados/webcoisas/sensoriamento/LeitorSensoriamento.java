71b66288b5feb28e38bedd51f2f9de1719e32708
manuela1111 <mn2192@yahoo.com.br>
2016-01-24 10:49:24 -0300

    Usando diretório relativo no projeto java

--- webcoisas/src/br/edu/ifba/embarcados/webcoisas/sensoriamento/LeitorSensoriamento.java
+++ webcoisas/src/br/edu/ifba/embarcados/webcoisas/sensoriamento/LeitorSensoriamento.java
@@ -8,7 +8,7 @@ public class LeitorSensoriamento implements
 	Runnable {
 
 	private static final String ARQUIVO_PIPE = 
-			"/home/~/Desenvolvimento/embarcados/logs/sensoriamentop";
+			"~/Desenvolvimento/embarcados/logs/sensoriamentop";
 	
 	private RandomAccessFile pipe = null;
 	private boolean continuar = true;
